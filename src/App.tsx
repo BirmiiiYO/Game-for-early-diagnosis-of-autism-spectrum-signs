5ce861a42a23042d28d090373676ed6441d35e27
BirmiiiYo <avramenkoa773@gmail.com>
2023-03-11 15:50:10 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+   <>
+    <h1>init components</h1>
+    <p>process var: {process.env.NOT_SECRET_CODE}</p></>
+  );
+}
+
+export default App;
