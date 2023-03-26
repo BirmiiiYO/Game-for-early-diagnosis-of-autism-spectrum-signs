d4a108b26b9ce49a683d93e16b235ec6cd857926
EalsyCoD <klitch0405@gmail.com>
2023-03-26 21:41:30 +0300

    moving eyes

---
+++ src/games/MovingEyes/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const MovingEyesImage = styled.img`
+height: 350px;
+object-fit: cover;
+width: 400px;
+`
+export const GifBlock = styled.div`
+text-align: center;
+`
