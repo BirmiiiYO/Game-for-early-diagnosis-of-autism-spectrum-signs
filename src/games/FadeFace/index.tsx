1db442714bbbf7d7f96be4ddb9f243346d9f50d4
Константин <kanstantsin.rahalski@gmail.com>
2023-03-26 16:25:12 +0300

    fade face

---
+++ src/games/FadeFace/index.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { GameContainer } from "@/styles"
+
+import faceImage from '../../assets/icon/Male-Face.png'
+import { Image } from "./styles"
+
+export const FadeFace = () => {
+
+  return (
+    <GameContainer>
+      <Image src={faceImage} alt='face' />
+    </GameContainer>
+  )
+}
