303ce7eeef0ce21e3ed28045f3181085731ebad3
Константин <kanstantsin.rahalski@gmail.com>
2023-03-26 16:26:27 +0300

    fix export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
+import { FadeFace } from '@/games/FadeFace/index'
 import { FillingRoom } from '@/games/FillingRoom/index'
 import { GeometricFigFade } from '@/games/GeometricFigFade/index'
 import { GeometricFigMove } from '@/games/GeometricFigMove/index'
 
-export { FillingRoom, GeometricFigFade, GeometricFigMove }
+export { FadeFace,FillingRoom, GeometricFigFade, GeometricFigMove }
