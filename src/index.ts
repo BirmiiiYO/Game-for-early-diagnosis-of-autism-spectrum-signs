4745e30295763dc4d362115886595f3fa0e55068
EalsyCoD <klitch0405@gmail.com>
2023-03-28 21:21:40 +0300

    Fixed paths for build

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import { Emotions } from '@/games/Emotions/index'
-import { FadeFace } from '@/games/FadeFace/index'
-import { FillingRoom } from '@/games/FillingRoom/index'
-import { GeometricFigFade } from '@/games/GeometricFigFade/index'
-import { GeometricFigMove } from '@/games/GeometricFigMove/index'
-import { MovingEyes } from '@/games/MovingEyes/index'
-import { VideoSequence } from '@/games/VideoSequence/index'
+import { Emotions } from './games/Emotions/index'
+import { FadeFace } from './games/FadeFace/index'
+import { FillingRoom } from './games/FillingRoom/index'
+import { GeometricFigFade } from './games/GeometricFigFade/index'
+import { GeometricFigMove } from './games/GeometricFigMove/index'
+import { MovingEyes } from './games/MovingEyes/index'
+import { VideoSequence } from './games/VideoSequence/index'
 
 export { 
   Emotions,
